f3f05a35f2de83464b87d2c71d0ad2b81e45ec6f
MarioDanielCeronMayoral <92552033+MarioDanielCeronMayoral@users.noreply.github.com>
2023-11-06 19:35:05 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Clase-107-Tarea
+# PROC106-V1-actividad-maestra1
+Python. OpenCV. Plantilla de la maestra.  
+  
+Lesson plan.  
+
+### Texto en inglés: PRO-C106-Teacher-Boilerplate
+PRO-C106-Teacher-Boilerplate
---
+++ live_detect.py
@@ -0,0 +1,41 @@
+# Importar la biblioteca OpenCV
+import cv2
+
+# Cargar el archivo Clasificador en cascadas
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+# Definir un objeto de captura de video
+vid = cv2.VideoCapture(0)
+
+while(True):
+   
+    # Capturar el video cuadro por cuadro
+    ret, frame = vid.read()
+
+    # Convertir a escala de grises
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detectar las caras
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    eyes = eye_cascade.detectMultiScale(gray,1.1,5)
+
+    # Dibujar los rectángulos al rededor de cada cara
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    for (x,y,w,h) in eyes:
+       cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+        
+    # Mostrar el cuadro resultante 
+    cv2.imshow('Fotograma', frame)
+      
+    # Salir de la ventana a través de la barra espaciadora
+    if cv2.waitKey(25) == 32:
+        break
+  
+# Después del bucle, liberar el objeto capturado
+vid.release()
+
+# Destruir todas las ventanas
+cv2.destroyAllWindows()
